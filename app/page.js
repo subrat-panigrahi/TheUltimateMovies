34cc001a76073389b71bf9b5eb6af49895ec1992
subrat <subrat.panigrahi@paytm.com>
2024-09-20 15:23:24 +0530

    ssg test

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import MovieListContainer from './homepage/container';
 import Header from './components/Header';
 // import { Suspense } from 'react';
 async function MoviesPage() {
-  const response = await fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&api_key=${process.env.TMDB_API_KEY}`,{ cache: 'no-store' });
+  const response = await fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&api_key=${process.env.TMDB_API_KEY}`);
   const movies = await response.json();
   return (
     <div>
