dec9031647748c6072f28da42295af7e4ebcaf77
subrat <subrat.panigrahi@paytm.com>
2024-09-21 04:24:20 +0530

    csp addition

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,30 @@ const nextConfig = {
         domains: ['image.tmdb.org'],
     },
         reactStrictMode: false,
+        async headers() {
+            return [
+              {
+                // Apply these headers to all routes
+                source: "/(.*)",
+                headers: [
+                  {
+                    key: "Content-Security-Policy",
+                    value: `
+                      default-src 'self';
+                      script-src 'self' 'unsafe-eval' 'unsafe-inline';
+                      style-src 'self' 'unsafe-inline';
+                      img-src 'self' 'image.tmdb.org';
+                      font-src 'self';
+                      connect-src 'self' 'image.tmdb.org';
+                      frame-ancestors 'none';
+                      object-src 'none';
+                      base-uri 'self';
+                    `.replace(/\s{2,}/g, ' ').trim()
+                  }
+                ]
+              }
+            ]
+          }
 };
 
 export default nextConfig;
