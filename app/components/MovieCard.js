e3a0885ca529d756a14f9c5ecfbbfff1aa55fbe0
subrat <subrat.panigrahi@paytm.com>
2024-09-21 19:55:10 +0530

    CLS update

--- app/components/MovieCard.js
+++ app/components/MovieCard.js
@@ -4,12 +4,11 @@
 import ImageBuilder from '../../lib/components/ImageBuilder';
 import TextLabel from '../../lib/components/TextLabel';
 export default function MovieCard({ movie, isEager }) {
-    console.log("isEager", isEager);
     return (
         <div className='max-w-4xl mx-auto p-4'>
             <div className="flex flex-col md:flex-row bg-white shadow-lg rounded-lg overflow-hidden">
                 <div className='md:w-1/3 p-2'>
-                <ImageBuilder src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} width='100' height='100' fallbackSrc='/images/movie.svg' alt={movie.title} className='w-full h-full object-cover' loading={isEager? 'eager': 'lazy'} />
+                <ImageBuilder src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} width='100' height='100' fallbackSrc='/images/movie.svg' alt={movie.title} className='w-full h-full object-cover' loading={isEager? 'eager': 'lazy'} priority={isEager}/>
                 </div>
                 <div className='md:w-2/3 p-6'>
                     <TextLabel label='Title' value={movie.title} />
