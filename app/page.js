8ba771a7e73f4d750d9c25d9c523f6519e122a9d
subrat <subrat.panigrahi@paytm.com>
2024-09-22 18:44:13 +0530

    revert

--- app/page.js
+++ app/page.js
@@ -1,25 +1,18 @@
-// import fetchWithTimeout from '@/lib/utils';
+import fetchWithTimeout from '@/lib/utils';
 import PopularMovies from './widgets/PopularMovies';
 import fetch from 'node-fetch';
-// import { MOVIES_DOMAIN } from '@/lib/constants';
+import { MOVIES_DOMAIN } from '@/lib/constants';
 
-/* async function fetchPopularMovies() {
-  try {
+async function fetchPopularMovies() {
     const response = await fetch(`${MOVIES_DOMAIN}/3/movie/popular?language=en-US&api_key=${process.env.TMDB_API_KEY}`, { next: { revalidate: 86400 } });
     let data = await response.json();
     return data;
-  }
-  catch (e) {
-    console.log("api call error", e);
-  }
-}; */
+};
 
 // Statically generated page
 export default async function MoviesPage() {
-  const response = await fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&api_key=52356ee2f53eb5738f1d63e73329f442`,{  // Revalidate every 24 hours
-    cache: 'force-cache'});
-  let data = await response.json();
-  
+  data = await fetchPopularMovies();
+
   return (
     <div>
       <PopularMovies movies={data} />
