f8d98e4f954bc643638a27172069da9c98fbd8de
subrat <subrat.panigrahi@paytm.com>
2024-10-10 20:46:41 +0530

    imported css

--- app/components/MovieCard.js
+++ app/components/MovieCard.js
@@ -5,8 +5,7 @@ import ImageBuilder from '../../lib/components/ImageBuilder';
 import TextLabel from '../../lib/components/TextLabel';
 export default function MovieCard({ movie, isEager }) {
     return (
-        <div className='max-w-4xl mx-auto p-4'>
-            hello
+        <div className='max-w-4xl mx-auto p-4'>            
             <div className="flex flex-col md:flex-row bg-white shadow-lg rounded-lg overflow-hidden">
                 <div className='md:w-1/3 bg-moviePoster bg-repeat-space'>
                 <ImageBuilder src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} width='100' height='100' fallbackSrc='/images/movie.svg' alt={movie.title} className='w-full h-full object-cover' loading={isEager? 'eager': 'lazy'} priority={isEager}/>
