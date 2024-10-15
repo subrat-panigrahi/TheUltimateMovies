7b7c31b99e6bf96a0ff373a58731ceab995d30bc
subrat <subrat.panigrahi@paytm.com>
2024-10-16 00:50:43 +0530

    new relic test2

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "./components/Header";
-// import Script from 'next/script';
+import Script from 'next/script';
 import { Suspense } from "react";
 
 const geistSans = localFont({
@@ -29,7 +29,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <script
+      <Script
+          id="newrelic-browser-agent"
           type="text/javascript"
           dangerouslySetInnerHTML={{
             __html: `
