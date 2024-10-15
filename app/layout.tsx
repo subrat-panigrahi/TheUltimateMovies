2fa3cabd393e2d7f8fa83e4fbc8bd874cd222046
subrat <subrat.panigrahi@paytm.com>
2024-10-16 00:44:24 +0530

    new relic test2

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "./components/Header";
-import Script from 'next/script';
+// import Script from 'next/script';
 import { Suspense } from "react";
 
 const geistSans = localFont({
